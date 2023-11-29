db97311fcb9241ade2d1ca9bdc5c7ede445e2973
azrulzuhrie <127113576+azrulzuhrie@users.noreply.github.com>
2023-11-29 22:03:18 +0700

    Add files via upload

---
+++ app/Config/DatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig
+{
+    // SETTING KONFIGURASI DATABASE KALIAN
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";  // You should provide the actual password here
+    public $database_name = "modul5";
+    public $port = 3306;
+}
+
+?>
